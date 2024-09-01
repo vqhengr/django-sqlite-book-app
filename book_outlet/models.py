2b48cf5d123aa70f8c599fe8e8ec5cd43f9b9c4f
vi <vi.ha.engr@gmail.com>
2024-08-31 20:16:18 -0700

    added author model

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 
+class Author (models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=100)
+ 
+    author = models.ForeignKey(Author , on_delete=models.CASCADE , null=True)
     is_bestseller = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     publisher = models.CharField(max_length=100)
@@ -18,6 +26,6 @@ class Book(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def get_absolute_url(self):
         return reverse("model_detail", kwargs={"pk": self.pk})
-    
+    slug = models.SlugField(max_length=255, unique=True , null=False, blank=True, db_index=True)
     def __str__(self):
         return self.title
